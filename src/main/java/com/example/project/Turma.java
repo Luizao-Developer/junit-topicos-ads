bbe87ebccd0566855d84864dc38009689ae0288d
Luiz <tteodoro1977@gmail.com>
2022-06-29 20:06:01 -0300

    Atividade_TopicosEspeciais

--- src/main/java/com/example/project/Turma.java
+++ src/main/java/com/example/project/Turma.java
@@ -37,4 +37,7 @@ public class Turma {
         this.notasMaiorQueMedia = maiorQueMedia.size();
         return this.notasMaiorQueMedia;
     }
+    public static void main(String args[]){
+        
+    }
 }
